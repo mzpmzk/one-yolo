db517e26853e6d1d6a839b8684a37a187d6e95f4
sherlockchou86 <zhouzhi9@foxmail.com>
2026-02-27 12:00:11 +0800

    add comment for YoloRuntime

--- include/YoloRuntime.h
+++ include/YoloRuntime.h
@@ -20,7 +20,26 @@ namespace yolo {
     public:
         YoloRuntime(const std::string& rt_name);
         ~YoloRuntime();
+        /**
+         * @brief
+         * inference based on different Yolo runtimes.
+         * 
+         * @param blob a 4D matrix to be sent to Yolo network.
+         * @return raw output matrixs from Yolo network, support multi-heads.
+         * 
+         * @note
+         * we use `cv::Mat` as the data structure to hold raw input & raw output for the inference, which acts like `Tensor` in `PyTorch` or other deep learning libraries.
+         * when using `cv::Mat` as a generic N-dimensional tensor (e.g., for deep learning inference or multi-dimensional array computation),
+         * you should discard OpenCV's traditional `image semantics` and adopt `array semantics` instead.
+        */
         virtual std::vector<cv::Mat> inference(const cv::Mat& blob) = 0;
+
+        /**
+         * @brief
+         * get description for the specific Yolo runtime.
+         * 
+         * @return description for the specific Yolo runtime, return `__rt_name` by default.
+        */
         virtual std::string to_string();
     };
 }
